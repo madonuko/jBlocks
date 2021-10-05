b67821ff65ecac216fe4d7b884e5fb17a42a363a
Hilderal Térence <terencehilderal@gmail.com>
2021-10-05 13:10:52 +0200

    initialize create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<h1>Hey Welcome to Jblocks</h1>
+			<p>
+				jBlocks is an online Japanese vocabulary and grammar learning app, based
+				on rearrangement/addition/modification of blocks.
+			</p>
+		</div>
+	);
+};
+
+export default App;
